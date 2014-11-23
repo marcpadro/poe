1964f935cf270cdd7bcf3702fd89eb795f3b4564
marcpadro <cramtnt@hotmail.com>
2014-11-23 14:07:50 +0100

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define ('IMG_QUEUE_DIR', '/users/cramtnt/documents/DIGEST_TEST/image_queue/');
-define ('TXT_QUEUE_DIR', '/users/cramtnt/documents/DIGEST_TEST/text_queue/');
-define ('GET_IMG_DIR', '/users/cramtnt/documents/DIGEST_TEST/image_result/');
-define ('GET_TXT_DIR', '/users/cramtnt/documents/DIGEST_TEST/text_result/');
+define ('IMG_QUEUE_DIR', '/var/tmp/DIGEST/image_queue/');
+define ('TXT_QUEUE_DIR', '/var/tmp/DIGEST/text_queue/');
+define ('GET_IMG_DIR', '/var/tmp/DIGEST/image_result/');
+define ('GET_TXT_DIR', '/var/tmp/DIGEST/text_result/');
 define ('IMG_WIDTH', 150);
 define ('IMG_HEIGHT', 200);
